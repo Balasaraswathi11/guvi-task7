4c65b261ff99ddef1f012365c70b9f92eb73005c
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-24 21:00:17 +0530

    details updated

--- c. Details using foreach method/country_details.js
+++ c. Details using foreach method/country_details.js
@@ -1,10 +1,18 @@
 function reqListener() {
     const countries=JSON.parse(this.responseText)
     console.log(countries);
-  }
-  
+    
+    countries.forEach(function(country){
+        const details = {
+            Name: country.name.common,
+            Capital: country.capital,
+            Flag: country.flags.png
+        };
+        console.log(details);
+    });
+}
   const req = new XMLHttpRequest();
   req.addEventListener("load", reqListener);
-  req.open("GET", "http://www.example.org/example.txt");
+  req.open("GET", "https://restcountries.com/v3.1/all");
   req.send();
   
