438baf17b15972327fe154fff5ff1d2713b42fbc
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-24 20:16:06 +0530

    population updated

--- b. Filtering Population/population.js
+++ b. Filtering Population/population.js
@@ -1,1 +1,14 @@
+function reqListener() {
+    const countries=JSON.parse(this.responseText)
+    console.log(countries);
+    const populationfilter=countries.filter(function(popu){if (popu.population<200000){return true}
+return false})
+console.log(populationfilter)
+  }
+  
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://restcountries.com/v3.1/all");
+  req.send();
+
 
