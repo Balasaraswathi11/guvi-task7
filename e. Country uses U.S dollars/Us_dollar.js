04e536b0701b8c8404c51de5ba430eb788216df8
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-24 22:26:50 +0530

    updated us dollar countries

--- e. Country uses U.S dollars/Us_dollar.js
+++ e. Country uses U.S dollars/Us_dollar.js
@@ -1,1 +1,15 @@
+function reqListener() {
+    const countries=JSON.parse(this.responseText)
+    console.log(countries);
+    const dollar = countries.filter((value)=> value.currencies && value.currencies.USD)
+dollar.forEach((countriesWithUSD)=> console.log(countriesWithUSD.name.common))
+}
+
+
+
+
+    const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://restcountries.com/v3.1/all");
+  req.send();
 
