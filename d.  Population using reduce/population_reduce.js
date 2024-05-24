c22c1ee26d13894eae55de9f1c964519de52fde9
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-24 21:19:47 +0530

    updated population reduce

--- d.  Population using reduce/population_reduce.js
+++ d.  Population using reduce/population_reduce.js
@@ -1,1 +1,15 @@
+function reqListener() {
+    const countries=JSON.parse(this.responseText)
+    console.log(countries);
+    const populationreduce=countries.reduce(function(accumulator,country){
+return accumulator+country.population
 
+    },0)
+    console.log("Total Population of Countries:" + populationreduce)
+  }
+  
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://restcountries.com/v3.1/all");
+  req.send();
+  
