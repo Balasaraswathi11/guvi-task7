99edfb1739a6a6f2c5854235b9ad31cdfc2234c9
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-24 19:08:18 +0530

    updatrd files

--- a.  Filtering Asian Countries/Asia.js
+++ a.  Filtering Asian Countries/Asia.js
@@ -1,1 +1,10 @@
-
+function reqListener() {
+    const  country=JSON.parse(this.responseText)
+    console.log(country);
+  }
+  
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://restcountries.com/v3.1/all");
+  req.send();
+  
