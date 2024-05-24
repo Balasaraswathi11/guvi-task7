f4813b22b7e3b4b66c156c7dab9b3ac4692b6876
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-24 20:19:54 +0530

    updated js

--- c. Details using foreach method/country_details.js
+++ c. Details using foreach method/country_details.js
@@ -1,1 +1,10 @@
-
+function reqListener() {
+    const countries=JSON.parse(this.responseText)
+    console.log(countries);
+  }
+  
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "http://www.example.org/example.txt");
+  req.send();
+  
